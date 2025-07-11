dc328414f1b6c47f8fa2f4ba87a54575b5a052e7
Hilal Muhammad <155317768+ihilalmuhammad@users.noreply.github.com>
2025-07-11 12:00:54 +0500

    loops/while & for loop

---
+++ 5th.py
@@ -0,0 +1,30 @@
+# loops
+# while loop
+# example of while loop
+h=0
+while h <=10:
+    print(h)
+    h+=1
+ 
+
+
+# example of while loop with
+# break statement
+i = 1
+while i < 10:
+    print(i)
+    if i == 5:
+        break
+    i += 1
+
+# for loops
+for i in range(1,10):
+    print(i)
+
+# for loops breaks statemet 
+for i in range(1,10):
+    if i == 5:
+        break
+    print(i)
+
+    
